2ddeb1ffff1972ccbfe055e26c64129412b0cde2
matvey <matvik.gurov@gmail.com>
2026-02-21 01:53:36 +0300

    re-balance nodes_downtime coefficients

--- src/core/search/nodes_downtime.py
+++ src/core/search/nodes_downtime.py
@@ -16,7 +16,7 @@ from src.core.data.models import Node
 
 Number = Union[int, float]
 
-PRIOR_DOWN = (4.0531142381893885e-06, 9.108212106296508e-05)  # alpha, beta or up/down formula
+PRIOR_DOWN = (2.3589191441985017e-06, 4.121957095269658e-05)  # alpha, beta or up/down formula
 PRIOR_ANNOUNCE = (1.0, 60 * 30)  # by default, expecting 1 announce in hour
 
 
